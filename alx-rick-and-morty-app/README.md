172f35cdcae3ec79548aa54aae1d95c5cfd31dd1
Khalil Hafz <khalilhafz@gmail.com>
2025-12-14 16:15:11 +0200

    task4

---
+++ alx-rick-and-morty-app/README.md
@@ -0,0 +1,2 @@
+# alx-graphql-0x01
+3. Application of GraphQL in React
---
+++ alx-rick-and-morty-app/graphql/apolloClient.ts
@@ -0,0 +1,14 @@
+
+
+
+import { ApolloClient, InMemoryCache, HttpLink} from "@apollo/client"
+
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: "https://rickandmortyapi.com/graphql"
+  }),
+  cache: new InMemoryCache()
+})
+
+export default client;
+
